b0eb0febefe6e069c704a7b8115986f2c1e66a85
dagmawibabi <1babidagi@gmail.com>
2022-07-03 20:43:45 +0300

    SortingSentence

--- LeetCode/SortingSentence.py
+++ LeetCode/SortingSentence.py
@@ -1,18 +1,13 @@
-def sortSentence(s):
-    a = s.split(" ");
-    unsorted = []
-    originalSentence = []
-    for i in a:
-        unsorted.append(i[-1:])
-    unsorted.sort();
-    for i in unsorted:
-        for j in a:
-            if j[-1:] == i:
-                originalSentence.append(j[:-1]);
-    properSentence = ""
-    for i in originalSentence:
-        properSentence += i + " ";
-    print(properSentence);
-    print(" ".join(originalSentence))
-sortSentence("This1 awesome3 is2");
-        
+class Solution(object):
+    def sortSentence(self, s):
+        unsorted = []
+        originalSentence = []
+        jumbledWords = s.split(" ");
+        for i in jumbledWords:
+            unsorted.append(i[-1:])
+        unsorted.sort()
+        for i in unsorted:
+            for j in jumbledWords:
+                if j[-1:] == i:
+                    originalSentence.append(j[:-1])
+        return " ".join(originalSentence)
