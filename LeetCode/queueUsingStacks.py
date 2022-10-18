078bcb6b38e9f3306702ec3db39f52f524cdb1cc
dagmawibabi <1babidagi@gmail.com>
2022-10-18 21:50:56 +0300

    queue using stacks

---
+++ LeetCode/queueUsingStacks.py
@@ -0,0 +1,29 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.A, self.B = [], []
+
+    def push(self, x):
+        self.A.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.B.pop()
+
+    def peek(self):
+        if not self.B:
+            while self.A:
+                self.B.append(self.A.pop())
+        return self.B[-1]
+
+    def empty(self):
+        return not self.A and not self.B
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
